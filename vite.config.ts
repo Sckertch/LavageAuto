ab000af86cec8039b42d118522551496db75602f
Sckertch <mleda971@gmail.com>
2026-06-24 14:34:52 +0200

    feat: update .gitignore for environment files and configure Vite server proxy for API

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://api.lavageauto.fr:8081',
+        changeOrigin: true,
+       },
     },
   },
 })
